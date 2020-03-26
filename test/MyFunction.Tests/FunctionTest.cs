0ceb4416cab9ead1fcc79a91975aba20c91713ed
Bo Flynn <bo@tcgplayer.com>
2020-03-26 14:34:38 -0400

    Add Github action to build and run unit tests
    Using a Github action, we can build and run unit tests on every pull
    request to master.  This helps us know that the commit is good before it
    gets merged in.

--- test/MyFunction.Tests/FunctionTest.cs
+++ test/MyFunction.Tests/FunctionTest.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Xunit;
-using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
-
-using MyFunction;
 
 namespace MyFunction.Tests
 {
@@ -16,8 +8,6 @@ namespace MyFunction.Tests
         [Fact]
         public void TestToUpperFunction()
         {
-
-            // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
             var upperCase = function.FunctionHandler("hello world", context);
